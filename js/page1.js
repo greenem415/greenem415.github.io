13b26ad559ac2fe5fef8491c7cd47e9415f8d2f6
greenem415 <greenem415@gmail.com>
2015-08-07 19:25:59 +0000

    first commit

---
+++ js/page1.js
@@ -0,0 +1,18 @@
+/* ("button").click(function(){
+    $(".youDie").fadeIn(3000);
+}); */
+
+$("#youDie").fadeOut("fast", function()
+{
+  $("#youDie").html("YOU DIE!").fadeIn("fast");
+});
+
+
+
+
+
+
+
+
+
+
---
+++ js/pages.js
@@ -0,0 +1,5 @@
+$( "#clickme" ).click(function() {
+  $( "#book" ).fadeIn( "slow", function() {
+    // Animation complete
+  });
+});
